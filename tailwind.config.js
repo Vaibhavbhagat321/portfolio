4de2dcc82af4a2b44bc6ae3eef4910c2216af551
vaibhav <vaibhav.bhagat321@gmail.com>
2024-04-22 17:01:24 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    screens: {
+      xsm: "350px",
+      sm: "550px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
+};
